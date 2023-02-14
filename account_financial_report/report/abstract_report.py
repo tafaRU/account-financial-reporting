f136d0e4c8fe9e93f190e999b39599ee00c68cbc
tafaRU <alex.comba@agilebg.com>
2023-02-14 12:59:27 +0100

    [IMP] account_financial_report: Open Items report, display residual currency in ending balance lines for account when all journal items have the same currency

--- account_financial_report/report/abstract_report.py
+++ account_financial_report/report/abstract_report.py
@@ -98,8 +98,9 @@ class AgedPartnerBalanceReport(models.AbstractModel):
                 move_line["amount_residual"] -= credit_amount[ml_id]
         return move_lines
 
-    def _get_accounts_data(self, accounts_ids):
+    def _get_accounts_data(self, accounts_ids, company_id):
         accounts = self.env["account.account"].browse(accounts_ids)
+        company = self.env["res.company"].browse(company_id)
         accounts_data = {}
         for account in accounts:
             accounts_data.update(
@@ -112,6 +113,12 @@ class AgedPartnerBalanceReport(models.AbstractModel):
                         "group_id": account.group_id.id,
                         "currency_id": account.currency_id or False,
                         "currency_name": account.currency_id.name,
+                        "foreign_currency_id": (
+                            account.currency_id.id
+                            if account.currency_id
+                            and account.currency_id != company.currency_id
+                            else False
+                        ),
                         "centralized": account.centralized,
                     }
                 }
